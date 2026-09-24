09dfb4af1b70adf1738da1549d0e8288ed78b069
Matthias Matousek <m@tou.io>
2026-09-24 10:30:19 +0200

    Update bloodied status based on average HP, rather than actual (max) HP

---
+++ scripts/bloodied.mjs
@@ -0,0 +1,27 @@
+import { getMinMaxAvgHp } from "./hp.mjs";
+
+export async function updateBloodiedDeathsDoor(actor) {
+    try {
+        const hpRange = await getMinMaxAvgHp(actor);
+        const hp = actor.system.attributes.hp;
+        const bloodiedThreshold = hp.effectiveMax - hpRange.averageHp / 2;
+        const isBloodied = hp.value <= bloodiedThreshold;
+
+        const effectId = "dnd5ebloodied000";
+        const effect = actor.effects.get(effectId);
+        if (!isBloodied) return effect?.delete();
+        if (effect) return effect;
+
+        return ActiveEffect.implementation.create({
+            _id: effectId,
+            img: CONFIG.DND5E.bloodied.img,
+            flags: { dnd5e: { isTemporary: true } },
+            name: game.i18n.localize(CONFIG.DND5E.bloodied.name),
+            statuses: ["bloodied"],
+            showIcon: CONST.ACTIVE_EFFECT_SHOW_ICON?.CONDITIONAL
+        }, { parent: actor, keepId: true });
+    } catch (error)
+    {
+        console.error(`Death's Door | Could not update Bloodied for ${actor.name}`, error);
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { addHpRangeToCombatTracker } from "./tracker.mjs"
 import { maximizeHp } from "./hp-max.mjs"
+import { updateBloodiedDeathsDoor } from "./bloodied.mjs"
 
 Hooks.on("renderCombatTracker", (app, element) => {
     void addHpRangeToCombatTracker(app, element);
@@ -7,4 +8,16 @@ Hooks.on("renderCombatTracker", (app, element) => {
 
 Hooks.on("createToken", (tokenDocument, options, userId) => {
     void maximizeHp(tokenDocument, options, userId);
+});
+
+Hooks.once("ready", () => {
+    const actorPrototype = CONFIG.Actor.documentClass.prototype;
+    const updateBloodied = actorPrototype.updateBloodied;
+
+    actorPrototype.updateBloodied = async function(options = {}) {
+        if (this.type === "npc" && this.system.attributes.hp.formula) {
+            return updateBloodiedDeathsDoor(this);
+        }
+        return updateBloodied.call(this, options);
+    };
 });
